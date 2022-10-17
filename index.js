c452874cd038e2a7c559a44b7ade026a587963df
unknown <logan.ashbaugh0001@gmail.com>
2022-10-17 09:53:41 -0700

    first prototype

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as THREE from './node_modules/three/src/Three.js';
-import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls'
+ import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 import { DragControls } from 'three/examples/jsm/controls/DragControls'
 
@@ -16,6 +16,7 @@ document.body.appendChild( renderer.domElement );
 
 scene.background = new THREE.Color( 0xd3d3d3 );
 
+const pairList = []; 
 const objects = []; // list of all loaded objects in the scene
 const loadedObjs = [];  // booleans checking if those objects are loaded
 
@@ -24,6 +25,16 @@ const material = new THREE.MeshBasicMaterial( { color: 0x444400 } );
 const cube = new THREE.Mesh( geometry, material );
 
 const gltfLoader = new GLTFLoader();
+
+// const controls = createControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.mouseButtons = {
+    // LEFT: THREE.MOUSE.PAN,
+    MIDDLE: THREE.MOUSE.DOLLY,
+    RIGHT: THREE.MOUSE.ROTATE
+}
+controls.enablePan = false;
 
 loadSpheres();
 
@@ -46,13 +57,13 @@ btn2.addEventListener("click", function () { loadModel(1) });
 const btn3 = document.getElementById("switchModel3");
 btn3.addEventListener("click", function () { loadModel(2) });
 
-const editText = document.get
+var editText = document.getElementById("nameID");
 
 // fix so that key presses work on last selected, and objects are toggleable
 function loadModel(n){
     var url;
     if(n === 0 && !loadedObjs[0]) {
-        url = '/Skull test 1.glb';
+        url = '/fancyskull.glb';
         loadedObjs[0] = !loadedObjs[0];
     } 
     if(n === 1 && !loadedObjs[1]) {
@@ -70,10 +81,24 @@ function loadModel(n){
     // may need to have array of objects later
     gltfLoader.load(url, (gltf) => {
         model = gltf.scene;
-        model.side = THREE.MeshLambertMaterial;
+        // model.side = THREE.MeshLambertMaterial;
+        // model.material = new THREE.MeshPhongMaterial({
+        //     color:      0xFFFFFF,
+        //     specular:   0xFFFBF2,
+        //     shininess:  60,
+        //     map:        THREE.Texture,
+        //     side:       THREE.DoubleSide
+        // });
+        model.material = new THREE.ShadowMaterial({opacity: .3, color: 0xFFFFFF});
         mesh = model.material;
         scene.add(model);
-        objects.push(model);
+        for(var i = 0; i < model.children.length; i++){
+            objects.push(model.children[i]);
+            const objPair = new Object();
+            objPair.model = model.children[i];
+            objPair.name = "Object " + (pairList.length + 1); 
+            pairList.push(objPair);
+        }
         const dragControls = new DragControls([model], camera, renderer.domElement);
     })
 }
@@ -91,10 +116,34 @@ function loadSpheres(){
     scene.add( dot );
 }
 
-// add lock-in functionality
+const color = 0xEBDCC7;
+const castColor = 0xEBDCC7;
 
-const directionalLight = new THREE.DirectionalLight( 0x404040, 10 ); // 0xFAEBD7, 20
+const directionalLight = new THREE.DirectionalLight( color, 3 ); // 0xFAEBD7, 0x404040 20
+
+var light2 = directionalLight.clone();
+directionalLight.castShadow = true;
+light2.castShadow = false;
+directionalLight.intensity = 3;
+light2.intensity = 0.5;
+
 scene.add( directionalLight );
+
+const bottomLight = new THREE.DirectionalLight( color, 1 );
+bottomLight.position.set(0, -10, 0);
+scene.add(bottomLight);
+
+const leftLight = new THREE.DirectionalLight( castColor, 1 );
+leftLight.position.set(-10, 0, 0);
+scene.add(leftLight);
+
+const rightLight = new THREE.DirectionalLight( castColor, 1 );
+rightLight.position.set(10, 0, 0);
+scene.add(rightLight);
+
+const frontLight = new THREE.DirectionalLight( color, 1 );
+frontLight.position.set(0, 0, 10);
+scene.add(frontLight);
 
 camera.position.z = 5;
 
@@ -104,6 +153,7 @@ function keyPressed(e){
 	switch(e.key) {
         case 'Enter':
             if(scene.autoRotate){
+                controls.enabled = true;
                 scene.autoRotate = false;
                 //load last rotation so that we can return to it
                 scene.rotation.x = rot.x;
@@ -111,6 +161,7 @@ function keyPressed(e){
                 scene.rotation.z = rot.z;
             } else {
                 scene.autoRotate = true;
+                controls.enabled = false;
                 //save rotation so that when its stopped it goes back to old coordinates
                 rot = {
                     'x': scene.rotation.x,
@@ -118,35 +169,59 @@ function keyPressed(e){
                     'z': scene.rotation.z
                 }
             }
-        case 'ArrowUp':
+            break;
+        case 'i':
             model.rotateX(-0.1);
             break;
-        case 'ArrowDown':
+        case 'k':
             model.rotateX(0.1);
             break;
-        case 'ArrowLeft':
+        case 'j':
             model.rotateY(-0.1);
             break;
-        case 'ArrowRight':
+        case 'l':
             model.rotateY(0.1);
             break;
-        case ' ':
-            console.log("cube time");
-            const object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
-
-            object.position.x = 0;
-            object.position.y = 0;
-            object.position.z = 0;
-
-            object.rotation.x = Math.random() * 2 * Math.PI;
-            object.rotation.y = Math.random() * 2 * Math.PI;
-            object.rotation.z = Math.random() * 2 * Math.PI;
-
-            object.scale.x = Math.random() + 0.5;
-            object.scale.y = Math.random() + 0.5;
-            object.scale.z = Math.random() + 0.5;
-
-            scene.add( object );
+        case 'u':
+            model.rotation.z -= 0.1;
+            break;
+        case 'o':
+            model.rotation.z += 0.1;
+            break;
+        case 'w':
+            model.position.z -= 0.1;
+            break;
+        case 's':
+            model.position.z += 0.1;
+            break;
+        case 'a':
+            model.position.x -= 0.1;
+            break;
+        case 'd':
+            model.position.x += 0.1;
+            break;
+        case 'q':
+            model.position.y -= 0.1;
+            break;
+        case 'e':
+            model.position.y += 0.1;
+            break;
+        case 'n':
+            controls.zoomIn();
+            break;
+        case 'm':
+            controls.zoomOut();
+            break;
+        case 'Tab':
+            // detect if out of range first
+            var mInd = findModelIndex(model);
+            if(e.shiftKey && mInd - 1 >= 0){
+                model = objects[mInd - 1];
+            }
+            else if(!e.shiftKey && mInd + 1 < objects.length){
+                model = objects[mInd + 1];
+            }
+            updateObjName(model);
             break;
 	}
 	e.preventDefault();
@@ -157,53 +232,144 @@ document.body.addEventListener('click', onDocumentMouseDown);
 
 // get the last clicked element
 function onDocumentMouseDown( event ) {
+// https://stackoverflow.com/questions/26250810/three-js-get-object-name-with-mouse-click
+    // console.log("start func");
+    // var getMeshes = function(parents) {
+    //     var meshes = [];
+    //     console.log("length: " + parents.length)
+    //     for (var i = 0; i < parents.length; i++) 
+    //     {
+    //         console.log("instance of: " + parents[i].type)
+    //         // console.log("instance check: " + parents[i].geometry)
+    //         if (parents[i].children.length > 0) {
+    //             meshes = meshes.concat(getMeshes(parents[i].children));
+                
+    //             console.log(getMeshes(parents[i].children))
+    //             console.log("more parents plz")
+    //         } else if (parents[i] instanceof THREE.Mesh) {
+    //             console.log("it's a mesh!")
+    //             meshes.push(parents[i]);
+    //         }
+    //     }
+    //     console.log("end loop");
+    //     return meshes;
+    // };
+    // function attributeValues(o) 
+    // {
+    //     var out = [];
+    //     for (var key in o) {
+    //         if (!o.hasOwnProperty(key))
+    //             continue;
+    //             out.push(o[key]);
+    //     }
+    //     return out;
+    // }
+    // var objects = attributeValues(this.o3dByEntityId);
+    // var meshes = getMeshes(scene.children);
+    const index = 6;
+    console.log("length: " + scene.children[index].children.length)
+    for(let i = 0; i < scene.children[index].children.length; i++){
+        console.log("type of number " + i + ": " + scene.children[index].children[i].geometry);
+    }
+
+    // console.log(meshes);
 
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+    // var canvasBounds = scene.rendererModelling.domElement.getBoundingClientRect();
+    // mouse.x = (event.clientX - canvasBounds.left / (canvasBounds.right - canvasBounds.left )) * 2 - 1;
+    // mouse.y = - (event.clientY - canvasBounds.top / (canvasBounds.bottom - canvasBounds.top )) * 2 + 1;
     raycaster.setFromCamera(mouse, camera);
 
-    const intersects = raycaster.intersectObjects(scene.children, true);
+    // const camInverseProjection = new THREE.Matrix4().getInverse(this.camera.projectionMatrix);
+    // const cameraPosition = new THREE.Vector3().applyMatrix4(camInverseProjection);
+    // const mousePosition = new THREE.Vector3(mouse.x, mouse.y, 1).applyMatrix4(camInverseProjection);
+    // const viewDirection = mousePosition.clone().sub(cameraPosition).normalize();
 
-    console.log(scene.children[1].children);
-    console.log("intersects: " + intersects);
+    // this.raycaster.set(cameraPosition, viewDirection);
+    
+    const intersects = raycaster.intersectObjects(scene.children[6].children, true);
+
+    // console.log("detecting: " + scene.children[2].children);
+    // console.log("intersects: " + intersects);
     if (intersects.length > 0) {
         console.log('clicked :o');
-        let n = new THREE.Vector3();
-        n.copy(intersects[0].face.normal);
-        n.transformDirection(intersects[0].object.matrixWorld);
+        // let n = new THREE.Vector3();
+        // n.copy(intersects[0].face.normal);
+        // n.transformDirection(intersects[0].object.matrixWorld);
         model = intersects[0].object;
+        console.log("type of obj: " + model.geometry);
+        console.log("x: " + model.position.x + " y: " + model.position.y + " z: " + model.position.z);
     }
 
     //check for locking in
-    if (checkLocationRange(model, dot, 1)){
-        model.position.x = dot.position.x;
-        model.position.y = dot.position.y;
-        model.position.z = dot.position.z;
-        console.log("fired");
+    if (checkLocationRange(model, dot, 0.5)){
+        // model.position.x = dot.position.x;
+        // model.position.y = dot.position.y;
+        // model.position.z = dot.position.z;
+        model.position.set(0,0,0);
+        // console.log("fired");
     }
+
+    updateObjName(model);
+
 }
 
 function checkLocationRange( obj1, obj2, range ){
-    if(obj1.position.x > obj2.position.x - range && obj1.position.x < obj2.position.x + range){
-        if(obj1.position.y > obj2.position.y - range && obj1.position.y < obj2.position.y + range){
-            if(obj1.position.z > obj2.position.z - range && obj1.position.z < obj2.position.z + range){
+    if(obj1.position.x >= obj2.position.x - range && obj1.position.x <= obj2.position.x + range){
+        if(obj1.position.y >= obj2.position.y - range && obj1.position.y <= obj2.position.y + range){
+            if(obj1.position.z >= obj2.position.z - range && obj1.position.z <= obj2.position.z + range){
+                console.log("bam");
                 return true;
             }
-            else {
-                return false;
-            }
-        }
-        else {
-            return false;
         }
     }
-    else {
-        return false;
-    }
+    return false;
 }
+
+function findModelName(model){
+    for(let i = 0; i < pairList.length; i++){
+        if(pairList.at(i).model === model){
+            return pairList.at(i).name;
+        }
+    }
+    return "No object found";
+}
+
+function findModelIndex(model){
+    for(let i = 0; i < objects.length; i++){
+        if(objects.at(i) === model){
+            return i;
+        }
+    }
+    return -2;
+}
+
+function updateObjName(model){
+    editText.innerHTML = findModelName(model);
+}
+
+function resizeCanvasToDisplaySize() {
+    const canvas = renderer.domElement;
+    // look up the size the canvas is being displayed
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+  
+    // adjust displayBuffer size to match
+    if (canvas.width !== width || canvas.height !== height) {
+      // you must pass false here or three.js sadly fights the browser
+      renderer.setSize(width, height, false);
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+  
+      // update any render target sizes here
+    }
+  }
 
 const animate = function () {
     requestAnimationFrame( animate );
+
+    resizeCanvasToDisplaySize();
 
     if(scene.autoRotate){
         scene.rotation.x += 0.01;
